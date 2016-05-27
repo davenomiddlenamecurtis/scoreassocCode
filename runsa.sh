196a113368937fbafc82dc8e18b4376627d31f71
Dave Curtis <d.curtis@ucl.ac.uk>
2016-05-27 08:17:48 +0100

    Update runsa.sh

--- runsa.sh
+++ runsa.sh
@@ -1,12 +1,19 @@
 if [ .$1 == . ]
 then
-echo Usage: runpscore.sh GENE
+echo Usage: runsa.sh GENE
+echo
+echo This is a simple script to demonstrate running scoreassoc using plink-seq files
+echo First set up a plink-seq project (see plink-seq documentation)
+echo Then edit the script to set your project name and your phenotype
 exit
 fi
 		
 PROJECT=SSS
 PHENO=scz
+# replace above lines with your project and phenotype names
+
 GENE=$1
+
 pseq ${PROJECT} v-view --geno  --phenotype ${PHENO} --gene $GENE > ${GENE}.dat
 pseq ${PROJECT} counts --annotate refseq --gene $GENE --phenotype ${PHENO} > ${GENE}.annot
 pscoreassoc ${GENE}.dat --annotfile ${GENE}.annot --weightfile funcWeights.txt --filterfile exclusions.txt --outfile ${GENE}.psao --minweight 80 --ldthreshold 0.7 --dorecessive
